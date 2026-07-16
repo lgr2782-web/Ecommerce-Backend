08a7342b899a5bbdac4b178888ed41699444c9da
Leroy Gomez Romero <lgr2782@gmail.com>
2026-07-16 15:01:05 -0600

    feat: integración con Neon y controlador de órdenes seguro

--- config/db.js
+++ config/db.js
@@ -1,12 +1,18 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
+/*const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+});*/
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Esto es obligatorio para que Neon acepte la conexión segura
+  }
 });
 
 pool.on('connect', () => {
